3f0dfd7713d700cd04da7206d4d86e9e0ddb3cca
Student Laptop User <sebastian.sarviala@gmail.com>
2016-09-16 20:22:51 +0300

    more tests of class Question

--- WordGame/src/test/java/com/mgseb/wordgame/domain/QuestionTest.java
+++ WordGame/src/test/java/com/mgseb/wordgame/domain/QuestionTest.java
@@ -1,38 +1,51 @@
 package com.mgseb.wordgame.domain;
 
+import junit.framework.Assert;
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertFalse;
 import org.junit.Before;
 import org.junit.Test;
 
 public class QuestionTest {
-
+    
     private Question question;
     private final String hint;
     private final String answer;
-
+    
     public QuestionTest() {
         this.hint = "The favorite fruit of a monkey.";
         this.answer = "Banana";
     }
-
+    
     @Before
     public void setUp() {
         this.question = new Question(hint, answer);
     }
-
+    
     @Test
     public void getHint() {
         assertEquals(hint,
                 question.getHint());
     }
-
+    
     @Test
     public void getAnswer() {
         assertEquals(answer, question.getAnswer());
     }
-
+    
     @Test
     public void isCorrect() {
-        assertEquals(true, question.isCorrect(answer));
+        assertTrue(question.isCorrect(answer));
+    }
+    
+    @Test
+    public void isCorrectCanReturnFalse() {
+        assertFalse(question.isCorrect(hint));
+    }
+    
+    @Test
+    public void isCorrectIsCaseInsensitive() {
+        assertTrue(question.isCorrect(answer.toUpperCase()));
     }
 }
