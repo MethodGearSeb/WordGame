ce42c28bae6f92d7daca826d5a1d83e9664afd91
Student Laptop User <sebastian.sarviala@gmail.com>
2016-09-23 18:24:49 +0300

    annoying error

--- WordGame/src/main/java/com/mgseb/wordgame/Main.java
+++ WordGame/src/main/java/com/mgseb/wordgame/Main.java
@@ -2,13 +2,21 @@ package com.mgseb.wordgame;
 
 import com.mgseb.wordgame.game.App;
 import com.mgseb.wordgame.game.QuestionReader;
+import com.mgseb.wordgame.game.QuestionSeries;
 import com.mgseb.wordgame.ui.ConsoleUI;
 import com.mgseb.wordgame.ui.UI;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("test0");
         QuestionReader reader = new QuestionReader(null);
+        QuestionSeries series = new QuestionSeries();
+        System.out.println("test0");
+        reader.readQuestions(series);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(series.next());
+        }
         UI ui = new ConsoleUI();
         App app = new App(reader, ui);
     }
--- WordGame/src/main/java/com/mgseb/wordgame/game/QuestionReader.java
+++ WordGame/src/main/java/com/mgseb/wordgame/game/QuestionReader.java
@@ -2,6 +2,7 @@ package com.mgseb.wordgame.game;
 
 import com.mgseb.wordgame.domain.Question;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class QuestionReader {
@@ -17,26 +18,32 @@ public class QuestionReader {
         this.fileAddress = replaceIfInvalid(fileAddress);
     }
 
-    public void ReadQuestions(QuestionSeries series) {
+    public void readQuestions(QuestionSeries series) {
         Scanner scanner = openScanner();
+        
         if (scanner == null) {
             return;
         }
+        
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
             String hint = "";
             String answer = "";
+            
             if (line.contains("@")) {
                 hint = "";
+                
                 while (!line.contains("#")) {
                     line = scanner.nextLine();
                     hint += line;
                 }
             }
+            
             if (line.contains("#")) {
                 line = scanner.nextLine();
                 answer = line;
             }
+            
             Question question = new Question(hint, answer);
             series.addQuestion(question);
         }
@@ -48,15 +55,16 @@ public class QuestionReader {
                 return fileAddress;
             }
         }
-        return ""; // TODO: Add default fileAddress
+        return "recourses/wordgame/english.txt";
     }
 
     private Scanner openScanner() {
         File file = new File(fileAddress);
         Scanner scanner = null;
+        System.out.println("text1");
         try {
             scanner = new Scanner(file);
-        } catch (Exception e) {
+        } catch (FileNotFoundException e) {
             System.out.println("ERROR: File could not be found");
         }
         return scanner;
