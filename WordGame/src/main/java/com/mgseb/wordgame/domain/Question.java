444e9ce59a02cd7af1e9854002e844e8e7619e7d
Student Laptop User <sebastian.sarviala@gmail.com>
2016-10-06 17:23:07 +0300

    wrote JavaDoc for class Question

--- WordGame/src/main/java/com/mgseb/wordgame/domain/Question.java
+++ WordGame/src/main/java/com/mgseb/wordgame/domain/Question.java
@@ -3,12 +3,21 @@ package com.mgseb.wordgame.domain;
 import com.mgseb.wordgame.game.Difficulty;
 import java.util.Random;
 
+/**
+ * Manages question-answer pairs. Conceals partial answers to varying extent.
+ * Checks whether player's input matches answer.
+ */
 public class Question {
 
     private final String question;
     private final String answer;
     private String partialAnswer;
 
+    /**
+     * Sets up class variables. Partial answer is initially fully visible.
+     * @param question Question string read from file by QuestionReader
+     * @param answer Answer string read from file by QuestionReader
+     */
     public Question(String question, String answer) {
         this.question = question;
         this.answer = answer;
@@ -19,15 +28,31 @@ public class Question {
         return question;
     }
 
+    /**
+     * Returns partially conceals, then returns partial answer. Extent of
+     * concealment depends on chosen game difficulty.
+     * @param difficulty
+     * @return partially concealed answer to serve as a hint to the player
+     */
     public String getPartialAnswer(Difficulty difficulty) {
         hideLetters(difficulty);
         return partialAnswer;
     }
 
+    /**
+     * Checks whether user input matches answer. Case insensitive.
+     * @param guess user input
+     * @return the correctness of the player's guess
+     */
     public boolean isCorrect(String guess) {
         return guess.toLowerCase().equals(answer.toLowerCase());
     }
 
+    /**
+     * Partially conceals partial answer. Number of hidden letters depends on
+     * chosen difficulty.
+     * @param difficulty 
+     */
     public void hideLetters(Difficulty difficulty) {
         partialAnswer = answer;
         int hiddenLetters = numberOfHiddenLetters(difficulty);
